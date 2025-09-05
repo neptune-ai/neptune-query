b96419f862cabe6242f4d49f6234e778a9d2f46b
Piotr Gabryjeluk <piotr.gabryjeluk@neptune.ai>
2025-09-05 15:37:34 +0200

    PY-233 Add @experimental decorator to fetch_metric_buckets

---
+++ src/neptune_query/internal/experimental.py
@@ -0,0 +1,50 @@
+#
+# Copyright (c) 2025, Neptune Labs Sp. z o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import functools
+import warnings
+from typing import (
+    Any,
+    Callable,
+)
+
+
+class ExperimentalWarning(UserWarning):
+    """Warning for use of experimental API elements."""
+
+
+# registry of functions already warned
+_warned_experimentals = set()
+
+
+def experimental(func: Callable) -> Callable:
+    """Decorator to mark functions as experimental.
+    It will result in a warning being emitted when the function is used
+    for the first time.
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> object:
+        if func not in _warned_experimentals:
+            warnings.warn(
+                f"{func.__qualname__} is experimental and may change or be removed "
+                "in a future minor release. Use with caution in production code.",
+                category=ExperimentalWarning,
+                stacklevel=2,
+            )
+            _warned_experimentals.add(func)
+        return func(*args, **kwargs)
+
+    return wrapper
