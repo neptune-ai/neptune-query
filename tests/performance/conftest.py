158dcedce4de8c5a117ea42b4529b77d6c98472c
Piotr Gabryjeluk <piotr.gabryjeluk@neptune.ai>
2025-09-26 16:40:36 +0200

    Performance tests (#70)
    * Performance tests
    
    * Parametrized performance tests plus BENCHMARK_PERFORMANCE_FACTOR
    
    * Remove warning supression
    
    * Adjust the expected thresholds
    
    * Rename the util module to decorator
    
    * More updates and fixes to perfomance testing
    
    * Fix parametrized performance benchmarks
    * More verbose messages
    * Properly silence pytest-benchmark warning on the validation phase
    
    * Fix aligmnment of detailed message to better work in GH UI
    
    * More beauty
    
    * Adjust thresholds
    
    * More updates
    
    * Always show the benchmark summary
    
    * Rename step to "Run performance tests"
    
    * Adjust meta params for the benchmarks
    
    * Fix format
    
    * Update docstrings
    
    * Add test for the perfomance issue introduced in nq 1.4.0
    
    * Change the way to specify multiple cases to @expected_benchmark
    
    * Rename PerfomanceCase to PerformanceTestCaseSpec
    
    * Fail tests properly
    
    * Reformat

---
+++ tests/performance/conftest.py
@@ -0,0 +1,60 @@
+import json
+import os
+import subprocess
+import sys
+import tempfile
+from io import BytesIO
+from pathlib import Path
+
+if not os.getenv("BENCHMARK_VALIDATE_FILE"):
+    # Create a temp dir for the benchmark results:
+    tmp_dir = tempfile.mkdtemp(prefix="neptune-query-benchmark-")
+    report_path = Path(tmp_dir) / "benchmark.json"
+
+
+def pytest_configure(config):
+    if not os.getenv("BENCHMARK_VALIDATE_FILE"):
+        # Perform at least 15 rounds per test
+        # Testing at least for 10 seconds per test
+        config.option.benchmark_min_rounds = 15
+        config.option.benchmark_max_time = 10.0
+        config.option.benchmark_disable_gc = True
+        config.option.benchmark_time_unit = "ms"
+        config.option.benchmark_sort = "name"
+        config.option.benchmark_json = BytesIO()
+
+
+def pytest_benchmark_update_json(config, benchmarks, output_json):
+    with open(report_path, "w") as f:
+        json.dump(output_json, f, indent=2)
+    with open("benchmark_results.json", "w") as f:
+        json.dump(output_json, f, indent=2)
+
+
+def pytest_sessionfinish(session, exitstatus):
+    try:
+        if exitstatus != 0:
+            return
+
+        if os.getenv("BENCHMARK_VALIDATE_FILE"):
+            return
+
+        if os.getenv("BENCHMARK_NO_VALIDATION") == "1":
+            return
+
+        # Rerun the tests in validation mode
+        os.environ["BENCHMARK_VALIDATE_FILE"] = str(report_path)
+        cp = subprocess.run(
+            [sys.executable] + sys.argv + ["-W", "ignore::pytest_benchmark.logger.PytestBenchmarkWarning"]
+        )
+        session.exitstatus = cp.returncode
+
+    finally:
+        try:
+            os.unlink(report_path)
+        except Exception:
+            pass
+        try:
+            os.rmdir(tmp_dir)
+        except Exception:
+            pass
