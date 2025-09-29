a96b4ea38fe3f246407f747adff4f4f7d0b791cf
Piotr Gabryjeluk <piotr.gabryjeluk@neptune.ai>
2025-09-29 14:25:19 +0200

    Disable test_perf_create_metric_buckets_dataframe

--- tests/performance/generate.py
+++ tests/performance/generate.py
@@ -11,7 +11,6 @@ from neptune_query.internal.identifiers import (
 )
 from neptune_query.internal.retrieval.attribute_types import FloatSeriesAggregations
 from neptune_query.internal.retrieval.attribute_values import AttributeValue
-from neptune_query.internal.retrieval.metric_buckets import TimeseriesBucket
 
 # Set the random seed for reproducibility
 random.seed(20250925)
@@ -50,14 +49,6 @@ def string_value(path: str, exp: int):
     )
 
 
-def bucket_metrics(experiments: int, paths: int, buckets: int) -> dict[RunAttributeDefinition, list[TimeseriesBucket]]:
-    return {
-        run_attribute_definition(experiment, path): [bucket_metric(index=i) for i in range(buckets)]
-        for experiment in range(experiments)
-        for path in range(paths)
-    }
-
-
 def run_attribute_definition(
     sys_id: int | str, path: int | str, attribute_type: str = "float_series"
 ) -> RunAttributeDefinition:
@@ -65,40 +56,3 @@ def run_attribute_definition(
         RunIdentifier(ProjectIdentifier("foo/bar"), SysId(f"sysid{sys_id}")),
         AttributeDefinition(f"path{path}", attribute_type),
     )
-
-
-def bucket_metric(index: int) -> TimeseriesBucket:
-    if index > 0:
-        return TimeseriesBucket(
-            index=index,
-            from_x=20.0 * index,
-            to_x=20.0 * (index + 1),
-            first_x=20.0 * index + 2,
-            first_y=100.0 * (index - 1) + 90.0,
-            last_x=20.0 * (index + 1) - 2,
-            last_y=100.0 * index,
-            y_min=80.0 * index,
-            y_max=110.0 * index,
-            finite_point_count=10 + index,
-            nan_count=5 - index,
-            positive_inf_count=2 * index,
-            negative_inf_count=index,
-            finite_points_sum=950.0 * index,
-        )
-    else:
-        return TimeseriesBucket(
-            index=index,
-            from_x=float("-inf"),
-            to_x=20.0,
-            first_x=20.0,
-            first_y=0.0,
-            last_x=20.0,
-            last_y=0.0,
-            y_min=0.0,
-            y_max=0.0,
-            finite_point_count=1,
-            nan_count=0,
-            positive_inf_count=0,
-            negative_inf_count=0,
-            finite_points_sum=0.0,
-        )
