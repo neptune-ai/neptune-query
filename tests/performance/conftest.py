dbf48c52ac056c13f39311da32ba5d6d720e9b04
Piotr Gabryjeluk <piotr.gabryjeluk@neptune.ai>
2025-09-30 12:34:38 +0200

    Separate XML file for measurement and validation

--- tests/performance/conftest.py
+++ tests/performance/conftest.py
@@ -22,9 +22,12 @@ def pytest_configure(config):
         config.option.benchmark_time_unit = "ms"
         config.option.benchmark_sort = "name"
         config.option.benchmark_json = BytesIO()
-        config.option.junit_prefix = "measurement"
+        config.option.junitxml = "benchmark_measurement.xml"
     else:
-        config.option.junit_prefix = "validation"
+        if config.option.xmlpath:
+            # For --junitxml = /path/abc.xml, create /path/abc__validation.xml
+            path = Path(config.option.xmlpath)
+            config.option.xmlpath = str(path.with_stem(path.stem + "__validation"))
 
 
 def pytest_benchmark_update_json(config, benchmarks, output_json):
