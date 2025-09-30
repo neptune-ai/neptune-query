42beb38b67c29cc62fb8b749270ee99647f87d70
Piotr Gabryjeluk <piotr.gabryjeluk@neptune.ai>
2025-09-30 20:44:18 +0200

    Generate more detailed JUnit reports (#93)
    * Generate more detailed JUnit reports
    
    * Add permission to e2e, add prefix to junit reports
    
    * Separate XML file for measurement and validation
    
    * Only run performance tests on main and dev branches

--- tests/performance/conftest.py
+++ tests/performance/conftest.py
@@ -22,6 +22,12 @@ def pytest_configure(config):
         config.option.benchmark_time_unit = "ms"
         config.option.benchmark_sort = "name"
         config.option.benchmark_json = BytesIO()
+        config.option.junitxml = "benchmark_measurement.xml"
+    else:
+        if config.option.xmlpath:
+            # For --junitxml = /path/abc.xml, create /path/abc__validation.xml
+            path = Path(config.option.xmlpath)
+            config.option.xmlpath = str(path.with_stem(path.stem + "__validation"))
 
 
 def pytest_benchmark_update_json(config, benchmarks, output_json):
